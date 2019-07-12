4a01d33a833db2666c6c42d1357c526b7c3ddf3f
sanu11 <sanikashah1110@gmail.com>
2019-07-11 18:01:55 -0700

    storing audio to ibm cloud

--- Django/mysite/scrum/views.py
+++ Django/mysite/scrum/views.py
@@ -20,6 +20,9 @@ import logging
 import smtplib
 from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
 import json
 
 import  json
@@ -68,8 +71,7 @@ def getRecording(request):
     file_name=data["file_name"]
     file_data=data["file_data"]
     audiofile_byte = base64.b64decode(file_data)
-    logger.debug(audiofile_byte)
-    logger.debug(file_name)
+
     path = '~/'
     # file = ContentFile(audiofile_byte)
     path=SITE_ROOT+'/static/recording.wav'
@@ -90,6 +92,66 @@ def writeToFile(data):
     path2=SITE_ROOT+'/static/recording.txt'
     with open(path2, "w+") as transcript:
         transcript.write(json.dumps(data, indent=2))
+
+def storeRecordingToCloud():
+
+    # Constants for IBM COS values
+    COS_ENDPOINT = "https://s3.us-south.cloud-object-storage.appdomain.cloud"  # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
+    COS_API_KEY_ID = "8OzW_kP6cYhXmbGT2k4gDvD6su5PJc-3g3_zNc2WPPXD"  # eg "W00YiRnLW4a3fTjMB-oiB-2ySfTrFBIQQWanc--P3byk"
+    COS_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
+    COS_RESOURCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/f55b867e510248799db6fbb07b7693d0:9ffac231-7c90-4168-a8ab-2578b4c056b6::"  # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003abfb5d29761c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
+
+    # Create resource
+
+    cos = ibm_boto3.resource("s3",
+                             ibm_api_key_id=COS_API_KEY_ID,
+                             ibm_service_instance_id=COS_RESOURCE_CRN,
+                             ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+                             config=Config(signature_version="oauth"),
+                             endpoint_url=COS_ENDPOINT
+                             )
+    multi_part_upload(cos,"hackathon-recordings","recording.wav",'/static/recording.wav')
+
+
+def multi_part_upload(cos,bucket_name, item_name, file_path):
+    try:
+        print("Starting file transfer for {0} to bucket: {1}\n".format(item_name, bucket_name))
+        # set 5 MB chunks
+        part_size = 1024 * 1024 * 5
+
+        # set threadhold to 15 MB
+        file_threshold = 1024 * 1024 * 15
+
+        # set the transfer threshold and chunk size
+        transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+            multipart_threshold=file_threshold,
+            multipart_chunksize=part_size
+        )
+
+        # the upload_fileobj method will automatically execute a multi-part upload
+        # in 5 MB chunks for all files over 15 MB
+        with open(file_path, "rb") as file_data:
+            cos.Object(bucket_name, item_name).upload_fileobj(
+                Fileobj=file_data,
+                Config=transfer_config
+            )
+
+        print("Transfer for {0} Complete!\n".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to complete multi-part upload: {0}".format(e))
+
+
+def get_item(cos,bucket_name, item_name):
+    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
+    try:
+        file = cos.Object(bucket_name, item_name).get()
+        print("File Contents: {0}".format(file["Body"].read()))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve file contents: {0}".format(e))
 
 
 def sendEmail():
